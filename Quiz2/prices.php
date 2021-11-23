4f420ddd29473a46b78c1478370cfee16d09ab8e
Tyler Haynes <72038702+haynet3@users.noreply.github.com>
2021-11-23 13:47:48 -0500

    Add files via upload

--- Quiz2/prices.php
+++ Quiz2/prices.php
@@ -29,22 +29,67 @@
   background-color: #04AA6D;
   color: white;
 }
+
+a.button1{
+ display:inline-block;
+ padding:0.35em 1.2em;
+ border:0.1em solid #FFFFFF;
+ margin:0 0.3em 0.3em 0;
+ border-radius:0.12em;
+ box-sizing: border-box;
+ text-decoration:none;
+ font-family:'Roboto',sans-serif;
+ font-weight:300;
+ color:#FFFFFF;
+ text-align:center;
+ transition: all 0.2s;
+}
+a.button1:hover{
+ color:#000000;
+ background-color:#FFFFFF;
+}
+@media all and (max-width:30em){
+ a.button1{
+  display:block;
+  margin:0.4em auto;
+ }
+}
+input[type=text] {
+    padding:5px; 
+    border:2px solid #ccc; 
+    -webkit-border-radius: 5px;
+    border-radius: 5px;
+}
+
+input[type=text]:focus {
+    border-color:#333;
+}
+
+input[type=submit] {
+    padding:5px 15px; 
+    background:#ccc; 
+    border:0 none;
+    cursor:pointer;
+    -webkit-border-radius: 5px;
+    border-radius: 5px; 
+}
+}
 </style>
 <body>
 <h1>Item Prices<h1>
     <form action="one.php" method="get">
-  <input type="submit" value="All items">
+  <input type="submit" class="button1" value="All items">
 </form>
 
 <h1>Items After Discount<h1>
     <form action="two.php" method="get">
-  <input type="submit" value="Discounts">
+  <input type="submit" class="button1" value="Discounts">
 </form>
 
 
 <h1>Average Item Prices<h1>
     <form action="three.php" method="get">
-  <input type="submit" value="Average price">
+  <input type="submit" class="button1" value="Average price">
 </form>
     
 
