01ae11a2a404fca8f7656c552134ecdcafefe196
Tyler Haynes <72038702+haynet3@users.noreply.github.com>
2021-11-23 13:44:52 -0500

    Add files via upload

---
+++ Quiz2/one.php
@@ -0,0 +1,52 @@
+<?php
+$con=mysqli_connect("localhost","root","","websys_quiz");
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+
+$result = mysqli_query($con,"SELECT * FROM items ORDER BY price desc");
+
+echo "<table border='1' id='customers'>
+<tr>
+<th>Name</th>
+<th>Price</th>
+</tr>";
+
+while($row = mysqli_fetch_array($result))
+{
+echo "<tr>";
+echo "<td>" . $row['name'] . "</td>";
+echo "<td>" . $row['price'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+
+?>
+<style>
+#customers {
+  font-family: Arial, Helvetica, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+#customers td, #customers th {
+  border: 1px solid #ddd;
+  padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+  padding-top: 12px;
+  padding-bottom: 12px;
+  text-align: left;
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
--- Quiz2/prices.php
+++ Quiz2/prices.php
@@ -31,94 +31,22 @@
 }
 </style>
 <body>
-    <h1>Item Prices<h1>
-<?php
-$con=mysqli_connect("localhost","root","","websys_quiz");
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+<h1>Item Prices<h1>
+    <form action="one.php" method="get">
+  <input type="submit" value="All items">
+</form>
 
-$result = mysqli_query($con,"SELECT * FROM items ORDER BY price desc");
-
-echo "<table border='1' id='customers'>
-<tr>
-<th>Name</th>
-<th>Price</th>
-</tr>";
-
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['name'] . "</td>";
-echo "<td>" . $row['price'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
-
-mysqli_close($con);
-
-?>
 <h1>Items After Discount<h1>
-<?php
-$con=mysqli_connect("localhost","root","","websys_quiz");
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
+    <form action="two.php" method="get">
+  <input type="submit" value="Discounts">
+</form>
 
-$result = mysqli_query($con,"SELECT items.name, items.price - (items.price * discounts.discount) AS Value FROM items, discounts WHERE items.id = discounts.item_id ORDER BY VALUE;");
-
-echo "<table border='1' id='customers'>
-<tr>
-<th>Name</th>
-<th>Price</th>
-</tr>";
-
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['name'] . "</td>";
-echo "<td>" . $row['Value'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
-
-mysqli_close($con);
-
-?>
 
 <h1>Average Item Prices<h1>
-    <?php
-$con=mysqli_connect("localhost","root","","websys_quiz");
-// Check connection
-if (mysqli_connect_errno())
-{
-echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-
-$result = mysqli_query($con,"SELECT items.name, (items.price + (items.price - (items.price * discounts.discount)))/2 AS Value FROM items, discounts WHERE items.id = discounts.item_id ORDER BY VALUE DESC;");
-
-echo "<table border='1' id='customers'>
-<tr>
-<th>Name</th>
-<th>Price</th>
-</tr>";
-
-while($row = mysqli_fetch_array($result))
-{
-echo "<tr>";
-echo "<td>" . $row['name'] . "</td>";
-echo "<td>" . $row['Value'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
-
-mysqli_close($con);
-
-?>
+    <form action="three.php" method="get">
+  <input type="submit" value="Average price">
+</form>
+    
 
 </body>
 </html>
